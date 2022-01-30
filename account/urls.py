1cddb56cd486436c8b701347b5e92561ca697521
Mitch Rosenlund <merosenlund@gmail.com>
2022-01-30 15:29:46 -0800

    feat: Add registration template files

--- account/urls.py
+++ account/urls.py
@@ -6,3 +6,14 @@ urlpatterns = [
     path("", views.dashboard),
     path("", include("django.contrib.auth.urls")),
 ]
+
+
+# URL PATTERNS INCLUDED IN "django.contrib.auth.urls"
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
