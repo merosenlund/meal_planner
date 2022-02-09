777a3f5450566d388023d439fab553e858e58672
Mitch Rosenlund <merosenlund@gmail.com>
2022-02-09 14:20:22 -0800

    Iss #1 - Refactor account to accounts

--- meal_planner/settings.py
+++ meal_planner/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "account.apps.AccountConfig",
+    "accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
