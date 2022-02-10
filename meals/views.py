74fe0b40c0813252edc3ba522f13a53345a41801
Mitch Rosenlund <merosenlund@gmail.com>
2022-02-10 08:56:02 -0800

    Iss #1 - Add recipe_list to urls and a basic template

--- meals/views.py
+++ meals/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def meal_list(request):
     return render(request, "meals/meal_list.html")
+
+
+def recipe_list(request):
+    return render(request, "meals/recipe_list.html")
