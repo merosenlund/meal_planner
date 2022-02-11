8148ecc0bb6b4008fae5eee29de12c6790dbb825
Mitch Rosenlund <merosenlund@gmail.com>
2022-02-10 17:35:39 -0800

    Iss #1 - All account templates added

--- meals/views.py
+++ meals/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def meal_list(request):
-    return render(request, "meals/meal_list.html")
+    return render(request, "meals/meal_list.html", {"section": "meals"})
 
 
 def recipe_list(request):
-    return render(request, "meals/recipe_list.html")
+    return render(request, "meals/recipe_list.html", {"section": "recipes"})
