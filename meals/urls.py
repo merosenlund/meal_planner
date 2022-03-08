8bfb54a6ffd148fc3687c456f376f0c4af818191
Mitch Rosenlund <merosenlund@gmail.com>
2022-03-08 12:41:35 -0800

    Random changes

--- meals/urls.py
+++ meals/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.meal_list, name="meals"),
     path("recipes/", views.recipe_list, name="recipes"),
-    path("schedule/", views.meal_form_view, name="schedule_meal"),
+    path("schedule/", views.meal_create_view, name="schedule_meal"),
     path("recipes/create/", views.recipe_create_view, name="create_recipe"),
     path("print/", views.print_meals, name="print_meal"),
 ]
