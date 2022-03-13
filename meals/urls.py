9253a13bd7b0baee24a5f34a900bbcb27f042b81
Mitch Rosenlund <merosenlund@gmail.com>
2022-03-12 21:48:35 -0800

    Add delete meal functionality

--- meals/urls.py
+++ meals/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.meal_list, name="meals"),
     path("<int:pk>/edit", views.MealEditView.as_view(), name="edit_meal"),
+    path("<int:pk>/delete", views.MealDeleteView.as_view(), name="delete_meal"),
     path("recipes/", views.recipe_list, name="recipes"),
     path("schedule/", views.meal_create_view, name="schedule_meal"),
     path("recipes/create/", views.recipe_create_view, name="create_recipe"),
